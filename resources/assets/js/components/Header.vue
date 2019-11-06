43105aab7f1736f6c9c4976f498b1ed8f4a97e5a
Sunnyboy Mathole <Matholeas@gmail.com>
2019-11-06 18:14:34 +0200

    created home components and vue store

---
+++ resources/assets/js/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+
+        <div class="container">
+            <router-link class="navbar-brand" to="/">My Vue App</router-link>
+
+            <button class="navbar-toggler" 
+                    type="button" 
+                    data-toggle="collapse" 
+                    data-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'app-header'
+    }
+</script>
---
+++ resources/assets/js/store.js
@@ -0,0 +1,13 @@
+export default {
+	state:{
+		welcomeMessage:'welcome to my vue spa',
+	},
+	mutations:{},
+	getters:{
+		welcome(state) 
+		{
+			return state.welcomeMessage;
+		}
+	},
+	actions:{}
+}	
